91ebdbdf4dfa2a2880fe305d73dcd88303f0d521
Ravi Patwardhan <ravipatwardhan123@gmail.com>
2025-02-28 10:25:27 -0500

    s

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agile-encouragement-production.up.railway.app']
 
 
 # Application definition
