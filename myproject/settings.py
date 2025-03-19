467e34e2f036f7683543ef9d10cea77a91348ff6
Ravi Patwardhan <ravipatwardhan123@gmail.com>
2025-03-19 18:36:11 -0400

    css

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['agile-encouragement-production.up.railway.app']
+ALLOWED_HOSTS = ['agile-encouragement-production.up.railway.app','localhost','127.0.0.1']
 #CSRF for Railway
 CSRF_TRUSTED_ORIGINS = ['https://agile-encouragement-production.up.railway.app']
 CSRF_COOKIE_SECURE = True
